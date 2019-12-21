c9f52ff2eea951735487024b83fe0b1bf43dfa37
Sathvik Malla <sathvikmalla17@gmail.com>
2019-12-21 15:07:24 -0800

    servo motor

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ServoMotor.java
@@ -0,0 +1,38 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name = "Concept: Scan Servo", group = "Concept")
+public class ServoMotor extends LinearOpMode {
+
+    Servo   servo;
+   // DCMotor motorTest;
+    @Override
+    public void runOpMode() {
+
+        double tgtPower = 0;
+        while (opModeIsActive()) {
+            tgtPower = -this.gamepad1.left_stick_y;
+          //  motorTest.setPower(tgtPower);
+            // check to see if we need to move the servo.
+            if (gamepad1.y) {
+                // move to 0 degrees.
+                servoTest.setPosition(0);
+            } else if (gamepad1.x || gamepad1.b) {
+                // move to 90 degrees.
+                servoTest.setPosition(0.5);
+            } else if (gamepad1.a) {
+                // move to 180 degrees.
+                servoTest.setPosition(1);
+            }
+            telemetry.addData("Servo Position", servoTest.getPosition());
+            telemetry.addData("Target Power", tgtPower);
+            telemetry.addData("Motor Power", motorTest.getPower());
+            telemetry.addData("Status", "Running");
+            telemetry.update();
+
+        }
+    }
+}
