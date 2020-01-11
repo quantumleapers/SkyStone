781281f07748a528c6557f4d0668e80ac317eac3
Samyak Anand <sam10.anand@gmail.com>
2020-01-11 13:43:31 -0800

    Vuforia
    Added support for detecting objects relative to robot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VuforiaNav.java
@@ -0,0 +1,127 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.vuforia.HINT;
+import com.vuforia.Vuforia;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.matrices.OpenGLMatrix;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackableDefaultListener;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
+
+/*
+ * This OpMode was written for the VuforiaDemo Basics video. This demonstrates basic principles of
+ * using VuforiaDemo in FTC.
+ */
+@Autonomous(name = "Vuforia")
+public class VuforiaNav extends LinearOpMode
+{
+    // Variables to be used for later
+    private VuforiaLocalizer vuforiaLocalizer;
+    private VuforiaLocalizer.Parameters parameters;
+    private VuforiaTrackables visionTargets;
+    private VuforiaTrackable target;
+    private VuforiaTrackableDefaultListener listener;
+
+    private OpenGLMatrix lastKnownLocation;
+    private OpenGLMatrix phoneLocation;
+
+    private static final String VUFORIA_KEY = "ASY9SwL/////AAABmTZ3pnbyjEw3saSYhW3UV3JEQfcQLkWuLWlyDVfBZWaakzc+Cp/II38n84AZQCMiIy9icNpNDMq1lQTTcn/J2RbzpSOiJlWsCgvs6ISgoHseeCTZJ1Rw0kIwE5/EVLvpRfUQEbO06WTcBBT0i/MBytjQunrA/Ewd3OjQt/tUiiJU8Kx/a343s2dewZmAx2J8XgYmXCv52P/r1dXzYk2PQ4Ep1ilA+juhTNu1L89zTT0hyVJvXSb5XrhmsmSPbOUfYcE6u9Jav13yPs+mbsWpYmYoDSsCEVm/XUNKlIQ0FsB2aXsVtZUSG2jtk5bSYIsDtsD45mgzn3gw6Juq229frEvs2UfjZ1DT3xr/fudtyUms";
+
+    private float robotX = 0;
+    private float robotY = 0;
+    private float robotAngle = 0;
+
+    public void runOpMode() throws InterruptedException
+    {
+        setupVuforia();
+
+        /*
+         We don't know where the robot is, so set it to the origin
+         If we don't include this, it would be null, which would cause errors later on
+        */
+        lastKnownLocation = createMatrix(0, 0, 0, 0, 0, 0);
+
+        waitForStart();
+
+        // Start tracking the targets
+        visionTargets.activate();
+
+        while(opModeIsActive())
+        {
+            // Ask the listener for the latest information on where the robot is
+            OpenGLMatrix latestLocation = listener.getUpdatedRobotLocation();
+
+            // The listener will sometimes return null, so we check for that to prevent errors
+            if(latestLocation != null)
+                lastKnownLocation = latestLocation;
+
+            float[] coordinates = lastKnownLocation.getTranslation().getData();
+
+            robotX = coordinates[0];
+            robotY = coordinates[1];
+            robotAngle = Orientation.getOrientation(lastKnownLocation, AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).thirdAngle;
+
+            // Send information about whether the target is visible, and where the robot is
+            telemetry.addData("Tracking " + target.getName(), listener.isVisible());
+            telemetry.addData("Last Known Location", formatMatrix(lastKnownLocation));
+
+            // Send telemetry and idle to let hardware catch up
+            telemetry.update();
+            idle();
+        }
+    }
+
+    private void setupVuforia()
+    {
+        /* Setup parameters to create localizer */
+        parameters = new VuforiaLocalizer.Parameters(R.id.cameraMonitorViewId); // To remove the camera view from the screen, remove the R.id.cameraMonitorViewId
+        parameters.vuforiaLicenseKey = VUFORIA_KEY;
+        parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
+        parameters.useExtendedTracking = false;
+        vuforiaLocalizer = ClassFactory.createVuforiaLocalizer(parameters);
+
+        /*
+         These are the vision targets that we want to use
+         The string needs to be the name of the appropriate .xml file in the assets folder
+        */
+        visionTargets = vuforiaLocalizer.loadTrackablesFromAsset("Skystone");
+        Vuforia.setHint(HINT.HINT_MAX_SIMULTANEOUS_IMAGE_TARGETS, 4);
+
+        // Setup the target to be tracked
+        target = visionTargets.get(0); // 0 corresponds to the stone target
+        target.setName("Stone Target");
+        target.setLocation(createMatrix(0, 500, 0, 90, 0, 90));
+
+        // Set phone location on robot
+        phoneLocation = createMatrix(0, 225, 0, 90, 0, 0);
+
+        // Setup listener and inform it of phone informa tion
+        listener = (VuforiaTrackableDefaultListener) target.getListener();
+        listener.setPhoneInformation(phoneLocation, parameters.cameraDirection);
+    }
+
+    /*
+     Creates a matrix for determining the locations and orientations of objects
+     Units are millimeters for x, y, and z, and degrees for u, v, and w
+    */
+    private OpenGLMatrix createMatrix(float x, float y, float z, float u, float v, float w)
+    {
+        return OpenGLMatrix.translation(x, y, z).
+                multiplied(Orientation.getRotationMatrix(
+                        AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES, u, v, w));
+    }
+
+    // Formats a matrix into a readable string
+    private String formatMatrix(OpenGLMatrix matrix)
+    {
+        return matrix.formatAsTransform();
+    }
+}
